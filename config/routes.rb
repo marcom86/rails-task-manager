7692ac47667239d983a9aa1b884fa8e1d5143621
Marco Montis <marcmontis7@gmail.com>
2020-03-13 23:53:57 +0100

    edit the task

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
 
-  get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
+
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
-
-
 end
